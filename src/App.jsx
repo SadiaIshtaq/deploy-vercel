8ba97272a54289d56d3a09435caf76c3e42055fe
SadiaIshtaq <sadiaishtaq21@gmail.com>
2025-06-19 15:52:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Sample Project</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero odio quas aut, similique, enim mollitia provident placeat iure rem tempora temporibus eius earum aspernatur facere dolore quia possimus voluptatibus velit, illum sint veritatis neque officiis! Illo nam autem ullam, ea enim fuga dolorum ab minima! </p>
+    </div>
+    </>
+  )
+}
+
+export default App
